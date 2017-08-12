5204eefa519c9a940d6dea910d3194237870aa85
mihail-kovashki <mihail.kovashki@yahoo.com>
2017-08-12 18:49:45 +0300

    Added Footer and fixed some links in navbar

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import {RouterModule} from '@angular/router';
 import {CommonModule} from '@angular/common';
 
 import {NavbarComponent} from './navbar.component';
+import {FooterComponent} from './footer.component';
 import {HttpService} from './http.service';
 import {AuthService} from './auth.service';
 import {MessageHandlerComponent} from './message-handler.component';
@@ -12,9 +13,9 @@ import {SpinnerService} from "./spinner/spinner.service";
 
 
 @NgModule({
-  declarations: [NavbarComponent, MessageHandlerComponent],
+  declarations: [NavbarComponent, MessageHandlerComponent, FooterComponent],
   imports: [RouterModule, CommonModule],
-  exports: [NavbarComponent, MessageHandlerComponent],
+  exports: [NavbarComponent, MessageHandlerComponent, FooterComponent],
   providers: [HttpService, AuthService, PrivateRoute, SpinnerService]
 })
 export class CoreModule {
